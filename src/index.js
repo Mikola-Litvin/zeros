d845f8d87bd92949b09538ee3b116cb15a22db21
Nikolai Shkrabkov <grr_by@mail.ru>
2019-10-08 01:20:56 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let exp = expression.split('*');
+  let arr = [];
+  let count, j;
+  let res = "1";
+  for (let i = 0; i < exp.length; i++) {
+
+    count = exp[i].match(/^[0-9]+/);
+    count = Number(count[0]);
+    j = exp[i].match(/[^0-9]+$/);
+    j = j[0].length;
+
+    if (j == 1) {
+      for (; j <= count; j++) {
+        arr.push(j);
+      }
+    }
+    else {
+      if (count % 2 == 0) {
+        for (j = 2; j <= count; j += 2) {
+          arr.push(j);
+        }
+      }
+      else {
+        for (j = 1; j <= count; j += 2) {
+          arr.push(j);
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    res = BigInt(res) * BigInt(arr[i]);
+  }
+
+  res = String(res);
+  res = res.match(/[0]+$/);
+
+  if (res == null) {
+    return 0;
+  }
+  else {
+    return res[0].length;
+  } 
 }
